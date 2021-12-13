1c3e727d66663d2aef49f1d2ee367c37ebf4cd9c
Sander Siimann <58694306+SanderSass@users.noreply.github.com>
2021-12-13 11:29:09 +0100

    update the basic layout

--- app/src/main/java/com/example/musicapplication_sp/MainActivity.kt
+++ app/src/main/java/com/example/musicapplication_sp/MainActivity.kt
@@ -2,10 +2,60 @@ package com.example.musicapplication_sp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.Toast
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.basic_main)
+
+        popupMenu()
+    }
+    private fun popupMenu()
+    {
+        val imageView = findViewById<ImageView>(R.id.iv_image)
+        val popupMenu = PopupMenu(applicationContext,imageView)
+        popupMenu.inflate(R.menu.menu)
+        popupMenu.setOnMenuItemClickListener {
+            when (it.itemId)
+            {
+                R.id.item1 -> {
+                    Toast.makeText(applicationContext, "Press 1", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.item2 -> {
+                    Toast.makeText(applicationContext, "Press 2", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.item3 -> {
+                    Toast.makeText(applicationContext, "Press 3", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> true
+            }
+        }
+
+        //hold on the image
+        imageView.setOnLongClickListener {
+
+            try {
+
+                val popup = PopupMenu::class.java.getDeclaredField("mPopup")
+                popup.isAccessible = true
+                val menu = popup.get(popupMenu)
+                menu.javaClass
+                    .getDeclaredMethod("setForceShowIcon", Boolean::class.java)
+                    .invoke(menu, true)
+            } catch (e: Exception)
+            {
+                e.printStackTrace()
+            } finally {
+                popupMenu.show()
+            }
+            true
+        }
     }
 }
