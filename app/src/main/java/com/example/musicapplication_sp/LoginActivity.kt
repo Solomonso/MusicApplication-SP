1a20d9c9669349f6419f7faaf60862992ceba8b1
solomon <Solomonn135@gmail.com>
2021-12-13 13:09:09 +0100

    implemented google sign in

--- app/src/main/java/com/example/musicapplication_sp/LoginActivity.kt
+++ app/src/main/java/com/example/musicapplication_sp/LoginActivity.kt
@@ -1,23 +1,36 @@
 package com.example.musicapplication_sp
 
-import android.app.Application
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.OAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
+
 class LoginActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "GoogleSignInActivity"
+        private const val RC_SIGN_IN = 9001
+    }
     private lateinit var auth: FirebaseAuth
     private lateinit var loginButton: Button
     private lateinit var loginEmail: EditText
     private lateinit var loginPassword: EditText
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var signInGoogleButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         auth = Firebase.auth
@@ -25,11 +38,87 @@ class LoginActivity : AppCompatActivity() {
         loginEmail = findViewById(R.id.login_email)
         loginPassword = findViewById(R.id.login_password)
         loginButton = findViewById(R.id.btn_login)
+        signInGoogleButton = findViewById(R.id.sign_in_button)
+
+        val provider = OAuthProvider.newBuilder("twitter.com")// construct an instance of twitter
+
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
         this.logIn()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        this.listenToClickForGoogleSignIn()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        Toast.makeText(
+            this@LoginActivity,
+            "Signed in with google successful " + currentUser!!.displayName,
+            Toast.LENGTH_SHORT
+        ).show()
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    //open the user profile
+                    //call a new intent
+                    val user = auth.currentUser
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Signed in with google successful " + user!!.displayName + " " + user.email,
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                }
+            }
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    private fun listenToClickForGoogleSignIn()
+    {
+        signInGoogleButton.setOnClickListener{
+            signIn()
+        }
     }
 
     /**
-     * @description This function is used for logging in users and validate is the email and password is valid
+     * @description This function is used for logging in users with validate is the email and password is valid
      */
     private fun logIn() {
         loginButton.setOnClickListener {
@@ -48,11 +137,19 @@ class LoginActivity : AppCompatActivity() {
 
                     auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
                         if (task.isSuccessful) {
-                            Toast.makeText(this@LoginActivity, "You are logged in " + auth.currentUser!!.email, Toast.LENGTH_SHORT).show()
+                            Toast.makeText(
+                                this@LoginActivity,
+                                "You are logged in " + auth.currentUser!!.email,
+                                Toast.LENGTH_SHORT
+                            ).show()
 
                             Log.e("Logged in", auth.currentUser!!.uid)
                         } else {
-                            Toast.makeText(this@LoginActivity, "Username or password not correct", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(
+                                this@LoginActivity,
+                                "Username or password not correct",
+                                Toast.LENGTH_SHORT
+                            ).show()
                         }
                     }
 
