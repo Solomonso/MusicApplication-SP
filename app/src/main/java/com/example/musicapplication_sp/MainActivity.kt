a81795db80c92bb214dd6dc627d9e21253401d9e
w-hulshoff <whulshoff@gmail.com>
2021-12-13 12:11:09 +0100

    screen orientation is always "portrait"

--- app/src/main/java/com/example/musicapplication_sp/MainActivity.kt
+++ app/src/main/java/com/example/musicapplication_sp/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_layout)
-        this.createSignIntent()
+//        this.createSignIntent()
     }
 
     private fun createSignIntent() {
