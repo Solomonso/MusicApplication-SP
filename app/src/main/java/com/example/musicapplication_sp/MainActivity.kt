6f366760bcf98ae01e4abf23f24bac6309479bbf
Sander Siimann <58694306+SanderSass@users.noreply.github.com>
2021-12-14 20:25:21 +0100

    registration Navigation still not workable

--- app/src/main/java/com/example/musicapplication_sp/MainActivity.kt
+++ app/src/main/java/com/example/musicapplication_sp/MainActivity.kt
@@ -2,63 +2,99 @@ package com.example.musicapplication_sp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import android.widget.ImageView
 import android.widget.PopupMenu
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+
+    @Override
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.basic_main)
-        //setSupportActionBar(toolbar)
 
-        popupMenu()
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        toggleDrawer()
+
+//        popUp()
     }
-    private fun popupMenu() {
 
-        val imageView = findViewById<ImageView>(R.id.iv_image)
-        imageView.setOnClickListener {
+    private fun toggleDrawer()
+    {
+        val menubar = findViewById<ImageView>(R.id.menubar)
+        menubar.setOnClickListener {
 
-            val popupMenu : PopupMenu = PopupMenu(this, imageView)
-            popupMenu.menuInflater.inflate(R.menu.menu,popupMenu.menu)
+            val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+            val navView : NavigationView = findViewById(R.id.navigation_view)
 
-            popupMenu.setOnMenuItemClickListener {
+            toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+            drawerLayout.addDrawerListener(toggle)
+            toggle.syncState()
+
+            navView.setNavigationItemSelectedListener {
                 when (it.itemId) {
-                    R.id.item1 -> {
-                        Toast.makeText(applicationContext, "Press 1", Toast.LENGTH_SHORT).show()
+                    R.id.profile -> {
+                        Toast.makeText(
+                            applicationContext,
+                            "Opened profile information",
+                            Toast.LENGTH_SHORT
+                        ).show()
                         true
                     }
-                    R.id.item2 -> {
-                        Toast.makeText(applicationContext, "Press 2", Toast.LENGTH_SHORT).show()
+                    R.id.playlist -> {
+                        Toast.makeText(applicationContext, "List of playlists", Toast.LENGTH_SHORT)
+                            .show()
                         true
                     }
-                    R.id.item3 -> {
-                        Toast.makeText(applicationContext, "Press 3", Toast.LENGTH_SHORT).show()
+                    R.id.settings -> {
+                        Toast.makeText(applicationContext, "Opened Settings", Toast.LENGTH_SHORT)
+                            .show()
+                        true
+                    }
+                    R.id.logout -> {
+                        Toast.makeText(applicationContext, "Logged Out", Toast.LENGTH_SHORT).show()
                         true
                     }
                     else -> true
                 }
             }
-
-            //hold on the image
-            imageView.setOnClickListener() {
-
-                try {
-
-                    val popup = PopupMenu::class.java.getDeclaredField("mPopup")
-                    popup.isAccessible = true
-                    val menu = popup.get(popupMenu)
-                    menu.javaClass
-                        .getDeclaredMethod("setForceShowIcon", Boolean::class.java)
-                        .invoke(menu, true)
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                } finally {
-                    popupMenu.show()
-                }
-                true
-            }
         }
+    }
+
+//    private fun popUp()
+//    {
+//        val clickList = findViewById<ImageView>(R.id.add_playlist)
+//        clickList.setOnClickListener {
+//
+//            val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+//            val addPlaylist : NavigationView = findViewById(R.id.navigation_addPlaylist)
+//
+//            toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+//            drawerLayout.addDrawerListener(toggle)
+//            toggle.syncState()
+//
+//            addPlaylist.setNavigationItemSelectedListener {
+//
+//            }
+//        }
+//    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
