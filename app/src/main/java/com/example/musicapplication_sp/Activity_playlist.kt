be999b71f618f26fd08a750258e13686b0473ede
Sander Siimann <58694306+SanderSass@users.noreply.github.com>
2021-12-16 11:24:44 +0100

    playlist form: Basic is still not workable, so I did not add basic elements on playlist form.

--- app/src/main/java/com/example/musicapplication_sp/Activity_playlist.kt
+++ app/src/main/java/com/example/musicapplication_sp/Activity_playlist.kt
@@ -2,11 +2,17 @@ package com.example.musicapplication_sp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 
 class Activity_playlist : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_playlist)
-    }
 
+        val playlists = findViewById<ListView>(R.id.txtPlaylist)
+
+    }
+    private fun readPlaylist(){
+
+    }
 }
