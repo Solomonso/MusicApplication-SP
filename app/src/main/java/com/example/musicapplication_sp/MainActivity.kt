800de0d11b40d023a0442629a3d2ae1caa764d3c
Ramonb2 <Ramon.brakels@student.stenden.com>
2021-12-20 12:07:24 +0100

    Registration added

--- app/src/main/java/com/example/musicapplication_sp/MainActivity.kt
+++ app/src/main/java/com/example/musicapplication_sp/MainActivity.kt
@@ -1,40 +1,36 @@
 package com.example.musicapplication_sp
 
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
-    private val signInLauncher = registerForActivityResult(
-        FirebaseAuthUIActivityResultContract()
-    ) { res ->
-        this.onSignInResult(res)
-    }
+
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_layout)
+        setContentView(R.layout.registration_layout)
+        auth = FirebaseAuth.getInstance()
+        auth.signOut()
+        signUp.setOnClickListener {
+            registerUser()
+        }
 //        this.createSignIntent()
     }
 
-    private fun createSignIntent() {
-        val providers = arrayListOf(
-            AuthUI.IdpConfig.GoogleBuilder().build(),
-            AuthUI.IdpConfig.TwitterBuilder().build(),
-            AuthUI.IdpConfig.GitHubBuilder().build()
-        )
 
-        // Create and launch sign-in intent
-        val signInIntent = AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .build()
-        signInLauncher.launch(signInIntent)
-        // [END auth_fui_create_intent]
-    }
+
 
     private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
         val response = result.idpResponse
@@ -49,4 +45,40 @@ class MainActivity : AppCompatActivity() {
             // ...
         }
     }
+
+    private fun registerUser() {
+        val email = editSignUpTextEmailAddress.text.toString()
+        val password = editSigUpTextPassword.text.toString()
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    auth.createUserWithEmailAndPassword(email, password)
+                    withContext(Dispatchers.Main) {
+                        //checkLoggedInState()
+                    }
+                } catch (e: Exception) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun checkLoggedInState() {
+        if (auth.currentUser == null) { // not logged in
+            tvLoggedIn.text = "You are not logged in"
+        } else {
+            tvLoggedIn.text = "You are logged in!"
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        checkLoggedInState()
+    }
+
 }
+
+
+
