74bbf3797b2ded564448de29d1712692b35663fe
solomon <Solomonn135@gmail.com>
2021-12-22 20:15:22 +0100

    fixed variable naming and commented out some code

--- app/src/main/java/com/example/musicapplication_sp/MainActivity.kt
+++ app/src/main/java/com/example/musicapplication_sp/MainActivity.kt
@@ -20,12 +20,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.registration_layout)
+        setContentView(R.layout.activity_registration)
         auth = FirebaseAuth.getInstance()
         auth.signOut()
-        signUp.setOnClickListener {
-            registerUser()
-        }
+//            registerUser()
+//        }
 //        this.createSignIntent()
     }
 
@@ -47,30 +46,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun registerUser() {
-        val email = editSignUpTextEmailAddress.text.toString()
-        val password = editSigUpTextPassword.text.toString()
-        if (email.isNotEmpty() && password.isNotEmpty()) {
-            CoroutineScope(Dispatchers.IO).launch {
-                try {
-                    auth.createUserWithEmailAndPassword(email, password)
-                    withContext(Dispatchers.Main) {
-                        //checkLoggedInState()
-                    }
-                } catch (e: Exception) {
-                    withContext(Dispatchers.Main) {
-                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
-                    }
-                }
-            }
-        }
+//        val email = editSignUpTextEmailAddress.text.toString()
+//        val password = editSigUpTextPassword.text.toString()
+//        if (email.isNotEmpty() && password.isNotEmpty()) {
+//            CoroutineScope(Dispatchers.IO).launch {
+//                try {
+//                    auth.createUserWithEmailAndPassword(email, password)
+//                    withContext(Dispatchers.Main) {
+//                        //checkLoggedInState()
+//                    }
+//                } catch (e: Exception) {
+//                    withContext(Dispatchers.Main) {
+//                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+//                    }
+//                }
+//            }
+//        }
     }
 
     private fun checkLoggedInState() {
-        if (auth.currentUser == null) { // not logged in
-            tvLoggedIn.text = "You are not logged in"
-        } else {
-            tvLoggedIn.text = "You are logged in!"
-        }
+//        if (auth.currentUser == null) { // not logged in
+//            tvLoggedIn.text = "You are not logged in"
+//        } else {
+//            tvLoggedIn.text = "You are logged in!"
+//        }
     }
 
     override fun onStart() {
