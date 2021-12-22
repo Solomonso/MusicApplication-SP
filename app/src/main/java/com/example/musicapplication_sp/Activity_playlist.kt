52b07af7e8d85471a7d70f3e0103e2dd50fa48d6
Sander Siimann <58694306+SanderSass@users.noreply.github.com>
2021-12-22 15:36:43 +0200

    Update

--- app/src/main/java/com/example/musicapplication_sp/Activity_playlist.kt
+++ app/src/main/java/com/example/musicapplication_sp/Activity_playlist.kt
@@ -2,17 +2,30 @@ package com.example.musicapplication_sp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.EditText
 import android.widget.ListView
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.database.DatabaseReference
 
 class Activity_playlist : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    lateinit var database: DatabaseReference
+    private lateinit var btnFab: FloatingActionButton
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_playlist)
 
-        val playlists = findViewById<ListView>(R.id.txtPlaylist)
-
-    }
-    private fun readPlaylist(){
-
+        btnFab.setOnClickListener { view ->
+            val alertDialog = AlertDialog.Builder(this)
+            val textEditText = EditText(this)
+            alertDialog.setMessage("Add Playlist item")
+            alertDialog.setTitle("Enter Plalist item")
+            alertDialog.setView(textEditText)
+            alertDialog.show()
+        }
     }
 }
---
+++ app/src/main/java/com/example/musicapplication_sp/PlaylistModel.kt
@@ -0,0 +1,13 @@
+package com.example.musicapplication_sp
+
+class PlaylistModel
+{
+    companion object Factory
+    {
+        fun createList(): PlaylistModel = PlaylistModel()
+    }
+
+    var UID: String? = null
+    var itemDatatext: String? = null
+    var delete: Boolean? = false
+}
