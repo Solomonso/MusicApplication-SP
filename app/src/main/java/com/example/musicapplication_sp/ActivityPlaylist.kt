5da6995f89910189a4f5748bf7c10306a2da5d87
Sander Siimann <58694306+SanderSass@users.noreply.github.com>
2021-12-23 14:15:10 +0200

    Update

--- app/src/main/java/com/example/musicapplication_sp/ActivityPlaylist.kt
+++ app/src/main/java/com/example/musicapplication_sp/ActivityPlaylist.kt
@@ -9,15 +9,18 @@ import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
-class Activity_playlist : AppCompatActivity() {
+class ActivityPlaylist : AppCompatActivity() {
 
-    lateinit var database: DatabaseReference
+//    lateinit var database: DatabaseReference
     private lateinit var btnFab: FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_playlist)
+
+//        database = FirebaseDatabase.getInstance().reference
 
         btnFab.setOnClickListener { view ->
             val alertDialog = AlertDialog.Builder(this)
@@ -25,7 +28,16 @@ class Activity_playlist : AppCompatActivity() {
             alertDialog.setMessage("Add Playlist item")
             alertDialog.setTitle("Enter Plalist item")
             alertDialog.setView(textEditText)
+//            alertDialog.setPositiveButton("Create playlist") { dialog, i ->
+//                val playlistItemData = PlaylistModel.createList()
+//                playlistItemData.itemDatatext =
+//            }
             alertDialog.show()
         }
+    }
+
+    private fun addPlaylistBox()
+    {
+
     }
 }
