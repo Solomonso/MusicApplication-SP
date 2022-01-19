208cf941b3e0ca42abfbb33e416a0436f6b61a85
Sander Siimann <58694306+SanderSass@users.noreply.github.com>
2022-01-19 11:02:17 +0100

    adding fetching from api

---
+++ app/src/main/java/com/example/musicapplication_sp/model/PostModel.kt
@@ -0,0 +1,7 @@
+package com.example.musicapplication_sp.model
+
+class PostModel(
+        val id: Int? = null,
+        val userID: String? = null,
+        val songName: String? = null
+)
---
+++ app/src/main/java/com/example/musicapplication_sp/repositories/OtherSongService.kt
@@ -0,0 +1,19 @@
+package com.example.musicapplication_sp.repositories
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object OtherSongService {
+    private val client = OkHttpClient.Builder().build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://musicapi.duckdns.org/api/songs/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    fun <T> buildService(service: Class<T>): T {
+        return retrofit.create(service)
+    }
+}
