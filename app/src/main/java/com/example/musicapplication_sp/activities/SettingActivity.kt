0818e0860f7662911cc958ee52eab2b69cbced8b
solomon <Solomonn135@gmail.com>
2022-02-08 19:02:48 +0100

    Made some update

--- app/src/main/java/com/example/musicapplication_sp/activities/SettingActivity.kt
+++ app/src/main/java/com/example/musicapplication_sp/activities/SettingActivity.kt
@@ -3,6 +3,7 @@ package com.example.musicapplication_sp.activities
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -10,12 +11,14 @@ import com.example.musicapplication_sp.R
 
 class SettingActivity : AppCompatActivity(){
     private lateinit var signSpotifyButton: Button
+    private lateinit var signOutSpotifyButton: Button
     private lateinit var sharedPreferences: SharedPreferences
     @Override
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
         signSpotifyButton = findViewById(R.id.sign_in_spotify_button)
+        signOutSpotifyButton = findViewById(R.id.sign_out_spotify_button)
         sharedPreferences = this.getSharedPreferences("Spotify", MODE_PRIVATE)
         this.listenToClickForSpotifySignInButton()
     }
@@ -27,7 +30,13 @@ class SettingActivity : AppCompatActivity(){
             if(username.isEmpty()) {
                 val intent = Intent(this@SettingActivity, SpotifyLogin::class.java)
                 startActivity(intent)
+                showUnlinkButton()
             } else {Toast.makeText(this@SettingActivity, "Spotify Account already linked.", Toast.LENGTH_SHORT).show()}
         }
+    }
+
+    private fun showUnlinkButton() {
+        signOutSpotifyButton.visibility = View.VISIBLE
+        signSpotifyButton.visibility = View.GONE
     }
 }
